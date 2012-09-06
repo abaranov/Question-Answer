fe34ed0bbf00ab698e53291ab3524962ca486dd0
Alexey Baranov <abaranov@productengine.com>
2012-09-06 13:32:51 +0300

    project Question/Answer

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuestionAnswer::Application.config.secret_token = 'b1de8ed4480d503026caec94016d533f7ba1c21462587320b05df8fb08b59a1594310c1e4a0cf74788315eedea69a2cc03912b4a2e72417d258c4a7b26cc364a'
