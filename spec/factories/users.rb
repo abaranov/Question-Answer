ac7941cac891d7e848a10e90ccce5475d29b1d40
Alexey Baranov <abaranov@productengine.com>
2012-09-12 19:05:22 +0300

    - add User model tests

---
+++ spec/factories/users.rb
@@ -0,0 +1,24 @@
+FactoryGirl.define do
+  factory :user do
+    sequence :email do |n|
+      "sperson#{n}@example.com"
+    end
+    password "FooBar8"
+    password_confirmation "FooBar8"
+  end
+
+  factory :admin, class: User do
+    sequence :email do |n|
+      "admin#{n}@example.com"
+    end
+    password "Admin8"
+    role "admin"
+  end
+
+  factory :question do
+    title "title"
+    body "text"
+    rate "0"
+    user  FactoryGirl.create(:user)
+  end
+end
